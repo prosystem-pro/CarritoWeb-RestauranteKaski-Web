2e333493a6105b33a8f36996fd07b927d6755858
prosystem-pro <prosystem490@gmail.com>
2025-07-22 06:41:49 -0600

    Nombre de la empresa corregida

--- src/app/Entornos/Entorno.prod.ts
+++ src/app/Entornos/Entorno.prod.ts
@@ -2,5 +2,5 @@ export const Entorno = {
   Produccion: true,
   ApiUrl: 'https://carritoweb-restaurantekaski-api-production.up.railway.app/api/',
   // ApiUrl: 'https://carritoweb-promesadios-api-production.up.railway.app/api/',
-  NombreEmpresa: 'CafeJuanAna'
+  NombreEmpresa: 'RestauranteKaski'
 };
--- src/app/Entornos/Entorno.ts
+++ src/app/Entornos/Entorno.ts
@@ -4,6 +4,6 @@ export const Entorno = {
     // ApiUrl: 'http://192.168.1.216:1433/api/',
     // ApiUrl: 'https://carritoweb-promesadios-api.onrender.com/api/',
     // ApiUrl: 'https://carritoweb-promesadios-api-production.up.railway.app/api/',
-    NombreEmpresa: 'CafeJuanAna'
+    NombreEmpresa: 'RestauranteKaski'
   };
   
